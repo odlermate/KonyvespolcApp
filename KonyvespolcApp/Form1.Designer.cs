62e829506b7429b2f3cdd96ffe3df7009b5e140c
odlermate <55752780+odlermate@users.noreply.github.com>
2019-11-24 23:03:44 +0100

    Add project files.

---
+++ KonyvespolcApp/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace KonyvespolcApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.label_ujkonyv = new System.Windows.Forms.Label();
+            this.konyv_text = new System.Windows.Forms.TextBox();
+            this.hozzaad_button = new System.Windows.Forms.Button();
+            this.mentes_button = new System.Windows.Forms.Button();
+            this.betoltes_button = new System.Windows.Forms.Button();
+            this.label_aktualmax = new System.Windows.Forms.Label();
+            this.label_per = new System.Windows.Forms.Label();
+            this.aktualis_num = new System.Windows.Forms.NumericUpDown();
+            this.max_num = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aktualis_num)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.max_num)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(120, 95);
+            this.lista.TabIndex = 0;
+            // 
+            // label_ujkonyv
+            // 
+            this.label_ujkonyv.AutoSize = true;
+            this.label_ujkonyv.Location = new System.Drawing.Point(12, 110);
+            this.label_ujkonyv.Name = "label_ujkonyv";
+            this.label_ujkonyv.Size = new System.Drawing.Size(52, 13);
+            this.label_ujkonyv.TabIndex = 1;
+            this.label_ujkonyv.Text = "Új könyv:";
+            // 
+            // konyv_text
+            // 
+            this.konyv_text.Location = new System.Drawing.Point(12, 126);
+            this.konyv_text.Name = "konyv_text";
+            this.konyv_text.Size = new System.Drawing.Size(100, 20);
+            this.konyv_text.TabIndex = 2;
+            // 
+            // hozzaad_button
+            // 
+            this.hozzaad_button.Location = new System.Drawing.Point(12, 152);
+            this.hozzaad_button.Name = "hozzaad_button";
+            this.hozzaad_button.Size = new System.Drawing.Size(75, 23);
+            this.hozzaad_button.TabIndex = 5;
+            this.hozzaad_button.Text = "Hozzáad";
+            this.hozzaad_button.UseVisualStyleBackColor = true;
+            this.hozzaad_button.Click += new System.EventHandler(this.hozzaad_button_Click);
+            // 
+            // mentes_button
+            // 
+            this.mentes_button.Location = new System.Drawing.Point(118, 152);
+            this.mentes_button.Name = "mentes_button";
+            this.mentes_button.Size = new System.Drawing.Size(75, 23);
+            this.mentes_button.TabIndex = 6;
+            this.mentes_button.Text = "Mentés";
+            this.mentes_button.UseVisualStyleBackColor = true;
+            this.mentes_button.Click += new System.EventHandler(this.mentes_button_Click);
+            // 
+            // betoltes_button
+            // 
+            this.betoltes_button.Location = new System.Drawing.Point(199, 152);
+            this.betoltes_button.Name = "betoltes_button";
+            this.betoltes_button.Size = new System.Drawing.Size(75, 23);
+            this.betoltes_button.TabIndex = 7;
+            this.betoltes_button.Text = "Betöltés";
+            this.betoltes_button.UseVisualStyleBackColor = true;
+            this.betoltes_button.Click += new System.EventHandler(this.betoltes_button_Click);
+            // 
+            // label_aktualmax
+            // 
+            this.label_aktualmax.AutoSize = true;
+            this.label_aktualmax.Location = new System.Drawing.Point(138, 12);
+            this.label_aktualmax.Name = "label_aktualmax";
+            this.label_aktualmax.Size = new System.Drawing.Size(78, 13);
+            this.label_aktualmax.TabIndex = 8;
+            this.label_aktualmax.Text = "Aktuális / Max.";
+            // 
+            // label_per
+            // 
+            this.label_per.AutoSize = true;
+            this.label_per.Location = new System.Drawing.Point(204, 33);
+            this.label_per.Name = "label_per";
+            this.label_per.Size = new System.Drawing.Size(12, 13);
+            this.label_per.TabIndex = 9;
+            this.label_per.Text = "/";
+            this.label_per.Click += new System.EventHandler(this.label_per_Click);
+            // 
+            // aktualis_num
+            // 
+            this.aktualis_num.Location = new System.Drawing.Point(141, 31);
+            this.aktualis_num.Name = "aktualis_num";
+            this.aktualis_num.Size = new System.Drawing.Size(52, 20);
+            this.aktualis_num.TabIndex = 10;
+            // 
+            // max_num
+            // 
+            this.max_num.Location = new System.Drawing.Point(222, 31);
+            this.max_num.Name = "max_num";
+            this.max_num.Size = new System.Drawing.Size(52, 20);
+            this.max_num.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 184);
+            this.Controls.Add(this.max_num);
+            this.Controls.Add(this.aktualis_num);
+            this.Controls.Add(this.label_per);
+            this.Controls.Add(this.label_aktualmax);
+            this.Controls.Add(this.betoltes_button);
+            this.Controls.Add(this.mentes_button);
+            this.Controls.Add(this.hozzaad_button);
+            this.Controls.Add(this.konyv_text);
+            this.Controls.Add(this.label_ujkonyv);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.aktualis_num)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.max_num)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label label_ujkonyv;
+        private System.Windows.Forms.TextBox konyv_text;
+        private System.Windows.Forms.Button hozzaad_button;
+        private System.Windows.Forms.Button mentes_button;
+        private System.Windows.Forms.Button betoltes_button;
+        private System.Windows.Forms.Label label_aktualmax;
+        private System.Windows.Forms.Label label_per;
+        private System.Windows.Forms.NumericUpDown aktualis_num;
+        private System.Windows.Forms.NumericUpDown max_num;
+    }
+}
+
---
+++ KonyvespolcApp/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KonyvespolcApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            aktualis_num.Maximum = 9999;
+            max_num.Maximum = 9999;
+        }
+
+        private void hozzaad_button_Click(object sender, EventArgs e)
+        {
+            if(konyv_text.Text == "")
+            {
+                Console.WriteLine("Nem adtad meg a könyv nevét!");
+                return;
+            }
+            lista.Items.Add(konyv_text.Text);
+        }
+
+        private void label_per_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void mentes_button_Click(object sender, EventArgs e)
+        {
+            if(aktualis_num.Value > max_num.Value)
+            {
+                MessageBox.Show("Az aktuális oldal nem lehet nagyobb, mint az összes oldal!"); ;
+                return;
+            }
+            if(aktualis_num.Value < 0 || max_num.Value < 0)
+            {
+                //ennek igazából semmi értelme, mert a numeric up down minimum értéke 0, de erre csak akkor jöttem rá mikor már megírtam.
+                MessageBox.Show("Az oldalak száma nem lehet negatív szám!");
+                return;
+            }
+            string kivalasztottkonyv = lista.SelectedItem.ToString();
+            int aktualis = Convert.ToInt32(aktualis_num.Value), max = Convert.ToInt32(max_num.Value);
+            var konyvek = new List<String>();
+            for (int i = 0; i < lista.Items.Count; i++)
+            {
+                lista.SetSelected(i, false);
+            }
+            for (int i = 0; i < lista.Items.Count; i++)
+            {
+                lista.SetSelected(i, true);
+                konyvek.Add(lista.SelectedItem.ToString());
+            }
+            StreamWriter sw = new StreamWriter("asd.txt");
+            sw.WriteLine(kivalasztottkonyv);
+            sw.WriteLine(aktualis);
+            sw.WriteLine(max);
+            foreach (var x in konyvek)
+            {
+                sw.WriteLine(x);
+            }
+            sw.Close();
+        }
+
+        private void betoltes_button_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("asd.txt");
+            string kivalasztottkonyv = sr.ReadLine().ToString();
+            int aktualis = Convert.ToInt32(sr.ReadLine());
+            int max = Convert.ToInt32(sr.ReadLine());
+            var konyvek = new List<String>();
+            while(!sr.EndOfStream)
+            {
+                konyvek.Add(sr.ReadLine().ToString());
+            }
+            aktualis_num.Value = aktualis;
+            max_num.Value = max;
+            foreach(var x in konyvek)
+            {
+                lista.Items.Add(x);
+            }
+            for (int i = 0; i < lista.Items.Count; i++)
+            {
+                if(lista.Items.IndexOf(i).ToString() == kivalasztottkonyv)
+                {
+                    lista.SetSelected(i, true);
+                }
+            }
+        }
+    }
+}
